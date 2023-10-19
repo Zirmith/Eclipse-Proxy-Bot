a63b10ea02e56430adbda05b8ceb2900f84cd58b
Zirmith <f2v74yt@gmail.com>
2023-10-19 14:59:43 -0500

    se

--- server.js
+++ server.js
@@ -1,35 +1,48 @@
+const express = require('express');
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 
+const app = express();
+const port = process.env.PORT || 3000;
+
 const apiUrl = 'http://pubproxy.com/api/proxy';
 const savePath = path.join(__dirname, 'proxies');
 
-// Make a request to the API
-axios.get(apiUrl)
-  .then((response) => {
-    // Check if the response data is as expected
-    if (response.data && response.data.data) {
-      const proxies = response.data.data;
-      if (proxies.length > 0) {
-        // Create the 'proxies' folder if it doesn't exist
-        if (!fs.existsSync(savePath)) {
-          fs.mkdirSync(savePath);
+function fetchAndSaveProxies() {
+  axios.get(apiUrl)
+    .then((response) => {
+      if (response.data && response.data.data) {
+        const proxies = response.data.data;
+        if (proxies.length > 0) {
+          if (!fs.existsSync(savePath)) {
+            fs.mkdirSync(savePath);
+          }
+          fs.writeFileSync(path.join(savePath, 'proxies.json'), JSON.stringify(proxies, null, 2), 'utf8');
         }
-
-        // Save the proxies as JSON in the 'proxies' folder
-        fs.writeFileSync(path.join(savePath, 'proxies.json'), JSON.stringify(proxies, null, 2), 'utf8');
-
-        // Now you can add code to check if the proxies work
-        // You can use the 'proxies' array for this purpose
-        console.log('Proxies have been saved.');
-      } else {
-        console.error('No proxies were retrieved from the API.');
       }
-    } else {
-      console.error('Invalid response from the API.');
-    }
-  })
-  .catch((error) => {
-    console.error('An error occurred:', error.message);
-  });
+    })
+    .catch((error) => {
+      console.error('An error occurred:', error.message);
+    });
+}
+
+const fetchInterval = 3600000; // 1 hour
+fetchAndSaveProxies();
+setInterval(fetchAndSaveProxies, fetchInterval);
+
+app.get('/proxies', (req, res) => {
+  // Read the saved proxies and send them as a response
+  const proxiesPath = path.join(savePath, 'proxies.json');
+  if (fs.existsSync(proxiesPath)) {
+    const proxies = JSON.parse(fs.readFileSync(proxiesPath, 'utf8'));
+    res.json(proxies);
+  } else {
+    res.status(404).json({ message: 'No proxies available.' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Proxy service is running on port ${port}`);
+  console.log(`Proxies will be fetched and saved every ${fetchInterval / 1000} seconds.`);
+});
